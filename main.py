209f7df8781cc24a3969d6dc3228465416eec6d4
Brian McGirk <bmcgirk2014@gmail.com>
2018-04-16 21:10:50 -0500

    Added a user model and foriegn key to link the two models

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://build-a-blog:buildablog@localhost:8889/build-a-blog'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://blogz:blogz@localhost:8889/blogz'
 app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 
@@ -12,12 +12,42 @@ class Blog(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(120))
     body = db.Column(db.String(200))
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, title, body):
+    def __init__(self, title, body, owner):
         self.title = title
         self.body = body
+        self.owner = owner
 
-  
+class User(db.Model):
+
+    id = db.Column(db.Integer, primary_key=True)
+    userName = db.Column(db.String(120))
+    password = db.Column(db.String(120))
+    blogs = db.relationship('Blog', backref='owner')
+
+    # TODO - Finish
+    pass
+
+@app.route("/signup")
+def signup():
+    # TODO - Finish
+    pass
+
+@app.route("/login")
+def login():
+    # TODO - Finish
+    pass
+
+@app.route("/index")
+def index():
+    # TODO - Finish
+    pass
+
+@app.route("/logout", methods=['POST'])
+def logout():
+    # TODO - del user from session
+    return redirect("/blog")
 
 @app.route("/blog", methods=['POST', 'GET'])
 def blog():
@@ -49,7 +79,8 @@ def new_post():
         if not blog_body:
             body_error = "Please fill out the body"
         if not title_error and not body_error:
-            new_blog = Blog(blog_title, blog_body)
+            owner = User.query.filter_by(email=session['email']).first()
+            new_blog = Blog(blog_title, blog_body, owner)
             db.session.add(new_blog)
             db.session.commit()
             return redirect("/blog?id={0}".format(new_blog.id))
@@ -58,11 +89,5 @@ def new_post():
 
     return render_template("/new_post.html")
             
-        
-
-    
-
-
-
 if __name__ == '__main__':    
     app.run()
